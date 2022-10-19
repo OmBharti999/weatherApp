e533e122fff18d60f28f649f532538f2622d584c
Om <ombharti888@gmail.com>
2022-10-19 15:22:48 +0000

    Update 2 files

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,45 +2,45 @@ import "./card.css";
 import { BiLocationPlus } from "react-icons/bi";
 import { TbWind } from "react-icons/tb";
 
-const data = {
-  request: {
-    type: "City",
-    query: "Deoghar, Bangladesh",
-    language: "en",
-    unit: "m"
-  },
-  location: {
-    name: "Deoghar",
-    country: "Bangladesh",
-    region: "",
-    lat: "24.267",
-    lon: "91.083",
-    timezone_id: "Asia/Dhaka",
-    localtime: "2022-10-19 19:25",
-    localtime_epoch: 1666207500,
-    utc_offset: "6.0"
-  },
-  current: {
-    observation_time: "01:25 PM",
-    temperature: 27,
-    weather_code: 113,
-    weather_icons: [
-      "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png"
-    ],
-    weather_descriptions: ["Clear"],
-    wind_speed: 13,
-    wind_degree: 313,
-    wind_dir: "NW",
-    pressure: 1008,
-    precip: 0,
-    humidity: 69,
-    cloudcover: 6,
-    feelslike: 30,
-    uv_index: 1,
-    visibility: 10,
-    is_day: "no"
-  }
-};
+// const data = {
+//   request: {
+//     type: "City",
+//     query: "Deoghar, Bangladesh",
+//     language: "en",
+//     unit: "m"
+//   },
+//   location: {
+//     name: "Deoghar",
+//     country: "Bangladesh",
+//     region: "",
+//     lat: "24.267",
+//     lon: "91.083",
+//     timezone_id: "Asia/Dhaka",
+//     localtime: "2022-10-19 19:25",
+//     localtime_epoch: 1666207500,
+//     utc_offset: "6.0"
+//   },
+//   current: {
+//     observation_time: "01:25 PM",
+//     temperature: 27,
+//     weather_code: 113,
+//     weather_icons: [
+//       "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0008_clear_sky_night.png"
+//     ],
+//     weather_descriptions: ["Clear"],
+//     wind_speed: 13,
+//     wind_degree: 313,
+//     wind_dir: "NW",
+//     pressure: 1008,
+//     precip: 0,
+//     humidity: 69,
+//     cloudcover: 6,
+//     feelslike: 30,
+//     uv_index: 1,
+//     visibility: 10,
+//     is_day: "no"
+//   }
+// };
 
 export default function Card({ data }) {
   return data ? (
@@ -51,7 +51,7 @@ export default function Card({ data }) {
           data.current.is_day == "no"
             ? "rgb(29, 33, 56)"
             : "rgb(221, 224, 148)",
-        color: data.current.is_day == "no" ? "white" : "black"
+        color: data.current.is_day == "no" ? "white" : "black",
       }}
     >
       <div className="top">
@@ -79,5 +79,12 @@ export default function Card({ data }) {
       </div>{" "}
       <div className="time">{data.current.observation_time}</div>
     </div>
-  ) : null;
+  ) : (
+    <div className="card-container">
+      {" "}
+      <div className="top">
+        <div className="img-container">Waiting for location</div>
+      </div>
+    </div>
+  );
 }
